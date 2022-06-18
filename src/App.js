bb0c401cf9e7f5fe88aad2f1696ac291b1b7e614
Betty de la Rosa <bertha.delarosa@live.com.mx>
2022-06-18 18:32:46 -0500

    Project created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleContainer from './Components/SimpleContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleContainer></SimpleContainer>
+    </div>
+  );
+}
+
+export default App;
